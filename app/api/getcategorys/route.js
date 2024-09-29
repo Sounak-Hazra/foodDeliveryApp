2343ea567f981f71b75363e0dc5077b1cd051e4c
Sounak-Hazra <hazrasounak87@gmail.com>
2024-09-30 02:20:00 +0530

    is it work

--- app/api/getcategorys/route.js
+++ app/api/getcategorys/route.js
@@ -9,7 +9,8 @@ export async function GET() {
         if (categorys.length === 0) {
             return NextResponse.json({ message: "Category not found", success: false }, { status: 400 });
         }
-        return NextResponse.json({ message: "Category found", data: categorys, success: true }, { status: 200 });
+        // Disable caching headers for this response
+        return NextResponse.json({ message: "Category found", data: categorys, success: true }, { status: 200, headers: { "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate" } });
     } catch (error) {
         console.log(error);
         return NextResponse.json({ message: "Category not found", success: false }, { status: 400 });
